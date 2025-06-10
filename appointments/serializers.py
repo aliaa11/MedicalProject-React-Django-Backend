48d77f68017d61e24b44deeccea1bc448e156009
Aliaa <aliaaalihesh@gmail.com>
2025-06-11 02:58:20 +0300

    Pationet connect done

--- appointments/serializers.py
+++ appointments/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from .models import Appointment
 
 class AppointmentSerializer(serializers.ModelSerializer):
+    doctor_name = serializers.CharField(source='doctor.user.username', read_only=True)
+    doctor_specialty = serializers.CharField(source='doctor.specialty.name', read_only=True)
+
     class Meta:
         model = Appointment
-        fields = '__all__'
+        fields = ['id', 'doctor', 'doctor_name', 'doctor_specialty', 'date', 'time', 'status']
