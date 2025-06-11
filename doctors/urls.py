97bb5dc6287c646156c567d8d7a855195df0ffbc
abdelrahman-abobakr <abdelrahmanabobakr321@gmail.com>
2025-06-11 14:33:39 +0300

    remove doctor app

--- doctors/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import DoctorListCreateView, DoctorRetrieveUpdateDestroyView, AvailableDoctorsListView
-
-urlpatterns = [
-    path('', DoctorListCreateView.as_view(), name='doctor-list-create'),
-    path('<int:pk>/', DoctorRetrieveUpdateDestroyView.as_view(), name='doctor-detail'),
-    path('available-doctors/', AvailableDoctorsListView.as_view(), name="available doctors")
-]
